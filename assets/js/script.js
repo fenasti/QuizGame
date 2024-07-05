3e1bce3f115a41d013aa8a11aa110d7f479cbeb3
fenasti zxc <td9z0vyv@students.codeinstitute.net>
2024-07-05 05:59:40 +0000

    access to the question element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//Start the game hidden the instruction box and displaying the questions
+
 const startBtn = document.getElementById("start-game");
 const questionBox = document.getElementById("question-box");
 const instructions = document.getElementById("instructions")
@@ -10,5 +12,44 @@ function startGame () {
     instructions.classList.add("hiden");
     questionBox.classList.remove("hiden");
     nextBtn.classList.remove("hiden");
+    displayQuestion()
 }
-   
+
+function displayQuestion() {
+    let question = quizQuestions[0].country;
+    console.log(question)
+}
+
+const quizQuestions = [
+    {
+        country: "France",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        correctAnswer: "Paris"
+    },
+    {
+        country: "Japan",
+        options: ["Beijing", "Tokyo", "Seoul", "Bangkok"],
+        correctAnswer: "Tokyo"
+    },
+    {
+        country: "Brazil",
+        options: ["Lima", "Brasilia", "Buenos Aires", "Santiago"],
+        correctAnswer: "Brasilia"
+    },
+    {
+        country: "Australia",
+        options: ["Sydney", "Canberra", "Melbourne", "Perth"],
+        correctAnswer: "Canberra"
+    },
+    {
+        country: "Canada",
+        options: ["Toronto", "Vancouver", "Ottawa", "Montreal"],
+        correctAnswer: "Ottawa"
+    },
+    {
+        country: "South Africa",
+        options: ["Pretoria", "Johannesburg", "Cape Town", "Durban"],
+        correctAnswer: "Pretoria"
+    }
+];
+
