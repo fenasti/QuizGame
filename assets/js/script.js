28bbaf2de64bd810ac08521beceb3afe25ad5108
fenasti zxc <td9z0vyv@students.codeinstitute.net>
2024-07-05 05:24:05 +0000

    Crete start function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const startBtn = document.getElementById("start-game");
+const questionBox = document.getElementById("question-box");
+const instructions = document.getElementById("instructions")
+const nextBtn = document.getElementById("next")
+
+startBtn.addEventListener("click", startGame)
+
+function startGame () {
+    startBtn.classList.add("hiden");
+    instructions.classList.add("hiden");
+    questionBox.classList.remove("hiden");
+    nextBtn.classList.remove("hiden");
+}
+   
